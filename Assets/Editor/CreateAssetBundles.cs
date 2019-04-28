7736a640b249e28e8d9abe51c0ad8f287499f17d
heqi <heqi.ju@unity3d.com>
2019-04-28 18:07:26 +0800

    add a menu item to check asset dependencies

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEditor;
+using UnityEngine;
 
 public class CreateAssetBundles
 {
@@ -14,5 +15,34 @@ public class CreateAssetBundles
     {
         string assetBundleDirectory = "Assets/AssetBundles";
         BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("Assets/Scene Objects Iteration")]
+    static void IterateSceneObjects()
+    {
+        object[] obj = GameObject.FindObjectsOfType(typeof(GameObject));
+        foreach(object o in obj)
+        {
+            GameObject g = (GameObject)o;
+            Debug.Log(g.name);
+        }
+    }
+
+    [MenuItem("Assets/Assets Dependencies")]
+    static void GetAssetsDependencies()
+    {
+        var obj = Selection.activeObject;
+        Object[] roots = new Object[] { obj };
+
+        var dep = EditorUtility.CollectDependencies(roots);
+        foreach(var o in dep)
+        {
+            if (AssetDatabase.Contains(o))
+            {
+                string assetPath = AssetDatabase.GetAssetPath(o);
+                Debug.Log(assetPath);
+            }
+        }
+        //Selection.objects = dep;
     }
 }
