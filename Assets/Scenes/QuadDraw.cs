82abe889c5a4e8e478ecdcb1acee4a7e2ee0252f
heqi <heqi.ju@unity3d.com>
2019-07-03 15:19:29 +0800

    add Quad draw class

---
+++ Assets/Scenes/QuadDraw.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadDraw : MonoBehaviour
+{
+    public Texture tex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (tex != null)
+        {
+            Graphics.Blit(tex, null as RenderTexture);
+        }
+    }
+}
